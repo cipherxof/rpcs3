e64d2067706feb9f6ec09f71c72510b12e044db8
Eladash <elad3356p@gmail.com>
2020-01-25 13:52:15 +0200

    sys_spu: Implement proper SPU group flags

--- rpcs3/Emu/Cell/SPUThread.cpp
+++ rpcs3/Emu/Cell/SPUThread.cpp
@@ -2869,7 +2869,7 @@ bool spu_thread::stop_and_signal(u32 code)
 
 		LOG_TRACE(SPU, "sys_spu_thread_receive_event(spuq=0x%x)", spuq);
 
-		if (group->type & SYS_SPU_THREAD_GROUP_TYPE_EXCLUSIVE_NON_CONTEXT) // this check may be inaccurate
+		if (!group->has_scheduler_context)
 		{
 			return ch_in_mbox.set_values(1, CELL_EINVAL), true;
 		}
